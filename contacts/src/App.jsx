a3cb6307efe9306a5e990177b025dbac8605748a
Anant <anantvijay.23@gmail.com>
2018-04-01 13:56:19 +0100

    Contacts Apps Building Start

--- contacts/src/App.jsx
+++ contacts/src/App.jsx
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ContactList from './ContactList.jsx';
+import ListContacts from './ListContacts';
+
+const contacts = [
+    {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+    },
+];
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <ContactList contacts={[
-                {name: 'Harry'},
-                {name: 'Hermoine'},
-                {name: 'Ron'}
-            ]}/>
-            <ContactList contacts={[
-                {name: 'Albus'},
-                {name: 'Severus'},
-                {name: 'James'}
-            ]} />
-        </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div>
+                <ListContacts contacts={contacts} />
+            </div>
+        )
+    }
+};
+
 
 export default App;
