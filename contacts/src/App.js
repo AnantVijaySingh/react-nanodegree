7d680cbbe72b96122aedeae8f8d3e5f5e2922f2a
Anant <anantvijay.23@gmail.com>
2018-04-07 12:28:23 +0100

    Changing App Screens without React Router.

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,46 +1,49 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 class App extends Component {
     state = {
-        contacts: [
-            {
-                "id": "karen",
-                "name": "Karen Isgrigg",
-                "handle": "karen_isgrigg",
-                "avatarURL": "http://localhost:5001/karen.jpg"
-            },
-            {
-                "id": "richard",
-                "name": "Richard Kalehoff",
-                "handle": "richardkalehoff",
-                "avatarURL": "http://localhost:5001/richard.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "handle": "tylermcginnis",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            },
-        ]
+        contacts: [],
+        screen: 'list'
     };
+
+    componentDidMount() {
+        ContactsAPI.getAll()
+            .then((contacts) => {
+            this.setState(() => ({contacts: contacts}))
+            })
+    }
 
     removeContact = (contact) => {
         this.setState(function (currentState) {
             return {
                     contacts: currentState.contacts.filter((c) => {return c.id !== contact.id})
                 }
-        })
+        });
+        ContactsAPI.remove(contact)
     };
-    // Parenthesize the body of function to return an object literal expression:
-    // params => ({foo: bar})
 
-render() {
+    createContact = () => {
+        this.setState(() => ({screen: 'create'}))
+    };
+
+    // Parenthesize the body of function to return an object literal expression:
+    // (params) => ({foo: bar})
+
+    render() {
         return (
             <div>
-                <ListContacts
-                    onDeleteContact={this.removeContact}
-                    contacts={this.state.contacts} />
+                { this.state.screen === 'list' && (
+                    <ListContacts
+                        onDeleteContact={this.removeContact}
+                        onNavigate={this.createContact}
+                        contacts={this.state.contacts} />
+                )}
+                { this.state.screen === 'create' && (
+                    <CreateContact/>
+                )}
             </div>
         )
     }
--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -22,7 +22,7 @@ class ListContacts extends React.Component {
     render() {
 
         const {query} = this.state;
-        const {contacts, onDeleteContact} = this.props;
+        const {contacts, onDeleteContact, onNavigate} = this.props;
 
         const showingContact = query ==='' ?
             contacts
@@ -38,6 +38,11 @@ class ListContacts extends React.Component {
                         value={query}
                         onChange={(event) => {this.updateQuery(event.target.value)}}
                     />
+                    <a
+                        href="#create"
+                        className="add-contact"
+                        onClick={onNavigate}
+                    >Create Contact</a>
                 </div>
                 {
                     showingContact.length !== contacts.length && ( /* && here is an operator that functions like a if statement*/
