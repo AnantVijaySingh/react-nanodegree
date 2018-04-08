96c7d3c63a215c5e326fcc71a0525393cc829eb1
Anant <anantvijay.23@gmail.com>
2018-04-08 18:55:20 +0100

    Contacts App

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             .then((contacts) => {
             this.setState(() => ({contacts: contacts}))
             })
-    }
+    };
 
     removeContact = (contact) => {
         this.setState(function (currentState) {
@@ -25,6 +25,14 @@ class App extends Component {
         ContactsAPI.remove(contact)
     };
 
+    createContact = (contact) => {
+        ContactsAPI.create(contact)
+            .then((contact) => {
+            this.setState((currentState) => ({
+                contacts: currentState.contacts.concat(contact)
+            }))
+            })
+    };
     // Parenthesize the body of function to return an object literal expression:
     // (params) => ({foo: bar})
 
@@ -38,8 +46,20 @@ class App extends Component {
                         onNavigate={this.createContact}
                         contacts={this.state.contacts} />
                 )} />
-                {/*We can simply use component prop instead of render if we want to pass a component that does not require and prop to be added*/}
-                <Route path="/create" component={CreateContact} />
+                {/*We can simply use component prop instead of render if we want to pass a component that does not require and prop to be added
+                <Route path="/create" component={CreateContact} /> */}
+
+                {/* ------ Route provides us with a history prop that has a push method that helps to change the URL to navigate the app -------- */ }
+
+                {/* We are also using an arrow function instead of simply passing this.createContact(contact) to the onCreateContact prop because we want to use the history prop to update the URL */ }
+                <Route path="/create" render={({ history }) => (
+                    <CreateContact
+                        onCreateContact={(contact) => {
+                            this.createContact(contact);
+                            history.push('/');
+                        }}
+                    />
+                )}  />
             </div>
         )
     }
